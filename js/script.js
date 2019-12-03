5fe699c98e10f49f06cbd7420c5e686409ff6916
Jonathan <jonathanpauluze@gmail.com>
2019-12-03 07:13:49 -0300

    add plugin config

---
+++ js/script.js
@@ -0,0 +1,14 @@
+if(window.SimpleSlide) {
+  new SimpleSlide({
+    slide: "quote",
+    time: 5000,
+    pauseOnHover: true
+  });
+  
+  new SimpleSlide({
+    slide: "portfolio",
+    nav: true,
+    time: 5000,
+    pauseOnHover: true
+  });
+}
